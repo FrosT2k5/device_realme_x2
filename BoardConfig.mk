5bb43d1e43f0cbba90fe3b485b051912531066ae
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-02-20 12:01:33 +0530

    RMX199X: Update defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@ DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/realme/RMX199X
-TARGET_KERNEL_CONFIG := vendor/x2_defconfig
+TARGET_KERNEL_CONFIG := vendor/RMX1992_defconfig
 #TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz-dtb
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
