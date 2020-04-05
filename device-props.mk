8135278da30c35ddc65c8e62a1fa51b571d0dcee
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-04-05 15:58:54 +0530

    RMX199X: drop media.setting
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>

--- device-props.mk
+++ device-props.mk
@@ -1,7 +1,3 @@
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
-
-# Media
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.settings.xml=/system/etc/media_profiles_vendor.xml
