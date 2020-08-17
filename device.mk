b36e87ffc5ee67995357c02fd3c39326f67c2598
Mohit Sethi <mohitsethi.32@gmail.com>
2020-08-17 11:49:51 +0530

    Initial Bliss Bringup.

--- device.mk
+++ device.mk
@@ -71,6 +71,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+PRODUCT_COPY_FILES += \
+   	vendor/bliss/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
